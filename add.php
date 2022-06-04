7eac79995a8b1a09f6dd0987256bfe2d29f93578
EvilBabyDemon <lteufelbe@ethz.ch>
2022-06-05 01:35:38 +0200

    fixed regex

--- add.php
+++ add.php
@@ -35,7 +35,7 @@
                     <legend>Review</legend>
                     <p>
                         <label for="course">Course number:</label><br>
-                        <input list="courses" id="course" name="course" placeholder="252-0027-00L" pattern="[A-Z0-9]{3}-[A-Z0-9]{4}-[A-Z0-9]{3} .*" size="80">
+                        <input list="courses" id="course" name="course" placeholder="252-0027-00L" pattern="[A-Z0-9]{3}-[A-Z0-9]{4}-[A-Z0-9]{3}( .*)?" size="80">
                         <datalist id="courses">
                             <?php
                             $db = new SQLite3('CourseReviews.db');
