5e100c6c0011288af5f46ebc1c9a583ea65f3773
EvilBabyDemon <lteufelbe@ethz.ch>
2022-06-05 22:04:17 +0200

    search +fts + check + redirect

--- index.php
+++ index.php
@@ -7,11 +7,26 @@
     <meta name="keywords" content="" />
     <meta name="description" content="" />
     <link href="../default.css" rel="stylesheet" type="text/css" />
+    <?php
+    if (isset($_POST["course"])) {
+        $course = substr($_POST["course"], 0, strpos($_POST["course"], " "));
+        $db = new SQLite3('CourseReviews.db');
+        $stmt = $db->prepare("SELECT NAME FROM COURSES WHERE COURSE=:course");
+        $stmt->bindParam(':course', $course, SQLITE3_TEXT);
+        $result = $stmt->execute();
+
+        if ($result->fetchArray()) {
+            echo "<meta http-equiv=\"Refresh\" content=\"0; url='https://n.ethz.ch/~lteufelbe/coursereview/$course/'\" />)";
+        }
+        $db->close();
+    }
+    ?>
+
 </head>
 
 <body>
     <div id="header">
-        <h1>CourseReview coming up</h1>
+        <h1>CourseReview</h1>
         <h2>&nbsp;</h2>
     </div>
     <div id="menu">
@@ -28,9 +43,43 @@
     ?>
     <div id="content">
         <div id="columnA">
+            <?php
+            if (isset($_POST["course"])) {
+                $db = new SQLite3('CourseReviews.db');
+                $stmt = $db->prepare("SELECT * FROM SEARCH WHERE SEARCH MATCH ':input' ORDER BY rank limit 10;");
+                $stmt->bindParam(':input', $_POST["course"], SQLITE3_TEXT);
+                $result = $stmt->execute();
+                echo "Your search didn't find an exact result, so here are the closest: <br>";
+                while ($row = $result->fetchArray()) {
+            ?>
+                    <a href="<?php echo "https://n.ethz.ch/~lteufelbe/coursereview/$row[0]/"; ?>"><?php echo "$row[0] $row[1]"; ?></a><br>
+            <?php
+                }
+                $db->close();
+            }
+            ?>
+            <form method="post" action="#">
+                <input list="courses" id="course" name="course" placeholder="Search for Reviews" pattern="[A-Z0-9]{3}-[A-Z0-9]{4}-[A-Z0-9]{3}( .*)?" size="40">
+                <datalist id="courses">
+                    <?php
+                    $db = new SQLite3('CourseReviews.db');
+                    $stmt = $db->prepare("SELECT * FROM COURSES;");
+                    $result = $stmt->execute();
+
+                    while ($row = $result->fetchArray()) {
+                    ?>
+                        <option value="<?php echo "$row[0] $row[1]"; ?>">
+                        <?php
+                    }
+                    $db->close();
+                        ?>
+                </datalist>
+                <input type="submit" value="Submit">
+            </form>
+
             <h2>Welcome <?php echo "$name $surname"; ?>!</h2>
             <p><b>CourseReview</b><br>
-                Everyone logged in should see this! </p>
+                Here you can add and read reviews of courses from ETHZ! </p>
             <a href="https://n.ethz.ch/~lteufelbe/coursereview/add.php">Add a review!</a> <br>
             <a href="https://n.ethz.ch/~lteufelbe/coursereview/edit.php">Edit your existent reviews!</a> <br>
             </p>
