0dae8bf5f3352c682159a9d6ea989972bf89ebdf
EvilBabyDemon <lteufelbe@ethz.ch>
2022-06-19 13:29:25 +0200

    added function to get new token

---
+++ newToken.php
@@ -0,0 +1,33 @@
+<?php
+function newToken() {
+    $ducky = "https://rubberducky.vsos.ethz.ch:1855/token";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CAINFO, "cacert-2022-04-26.pem");
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('accept: application/json' ,'Content-Type:application/x-www-form-urlencoded'));
+
+    $data = array(
+        'grant_type' => '',
+        'username' => trim(file_get_contents("secret/user.txt")),
+        'password' => trim(file_get_contents("secret/password.txt")),
+        'scope' => '',
+        'client_id' => '',
+        'client_secret' => '',
+    );
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    //$ducky = http_build_query($data);
+    curl_setopt($ch, CURLOPT_URL, $ducky);
+    $result = curl_exec($ch);
+
+    $result = json_decode($result, true);
+    
+    $token = $result['access_token'];
+    $secret = fopen("secret/key.txt", "w");
+    fwrite($secret, $token);
+    fclose($secret);
+    return $token;
+}
