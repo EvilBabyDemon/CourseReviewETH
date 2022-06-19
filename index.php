c7f006b2f2eb7903cbe2e12e2b9d6f8e962af83d
EvilBabyDemon <lteufelbe@ethz.ch>
2022-06-19 23:53:57 +0200

    added newest reviews to see

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+$token = file_get_contents("secret/key.txt");
+?>
 
 <head>
     <meta charset="utf-8">
@@ -10,7 +13,8 @@
     <link href="../default.css" rel="stylesheet" type="text/css" />
     <?php
     if (isset($_POST["course"])) {
-        $course = substr($_POST["course"], 0, strpos($_POST["course"], " "));
+        $course = $_POST["course"] . " ";
+        $course = substr($course, 0, strpos($course, " "));
         $db = new SQLite3('CourseReviews.db');
         $stmt = $db->prepare("SELECT NAME FROM COURSES WHERE COURSE=:course");
         $stmt->bindParam(':course', $course, SQLITE3_TEXT);
@@ -84,8 +88,56 @@
             <p>Here you can add and read reviews of courses from ETHZ!</p>
             <a href="https://n.ethz.ch/~lteufelbe/coursereview/add.php">Add a review!</a> <br>
             <a href="https://n.ethz.ch/~lteufelbe/coursereview/edit.php">Edit your existent reviews!</a> <br>
-            </p>
 
+            <?php
+            function getLatest(String $token)
+            {
+                $ducky = "https://rubberducky.vsos.ethz.ch:1855/latest";
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $ducky);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_CAINFO, "cacert-2022-04-26.pem");
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $token));
+                $result = curl_exec($ch);
+                $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                curl_close($ch);
+                if ($code == 401) {
+                    return true;
+                }
+                $js = json_decode($result);
+                $js = json_decode($js);
+                ?>
+                <br> Courses with the newest Reviews:
+                <ul>
+                    <?php
+                    foreach($js as $value){
+                        $fullname = $value->CourseNumber;
+                        $db = new SQLite3('CourseReviews.db');
+                        $stmt = $db->prepare("SELECT NAME FROM COURSES WHERE COURSE=:course");
+                        $stmt->bindParam(':course', $value->CourseNumber, SQLITE3_TEXT);
+                        $qresult = $stmt->execute();
+                        
+                        if ($row = $qresult->fetchArray()) {
+                            $fullname = $value->CourseNumber  . " ". $row[0];
+                        }
+                        $db->close();
+
+                        echo '<li><a href="https://n.ethz.ch/~lteufelbe/coursereview/' . htmlspecialchars($value->CourseNumber) . '/">' . htmlspecialchars("$fullname") . '</a></li>';
+                    }
+                    ?>
+                </ul>
+                <?php
+                return false;
+            }
+            
+            if (getLatest($token)) {
+                //get new token
+                require_once('newToken.php');
+                $token = newToken();
+                getLatest($token);
+            }
+            
+            ?>
         </div>
         <div id="footer">
             <p>If you think something is wrong or have any suggestion please contact me: <a href="mailto:lteufelbe@ethz.ch">lteufelbe@ethz.ch</a></p>
