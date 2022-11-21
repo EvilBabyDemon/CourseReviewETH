374b843a973d89d4fb18f5c8bb145d3f28dcaa82
lteufelbe <lteufelbe@ethz.ch>
2022-11-21 22:50:40 +0100

    opg meta tags for previews

---
+++ meta.php
@@ -0,0 +1,43 @@
+<?php 
+function getStats(String $token, String $api)
+{
+    $ducky = $api . "stats";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $ducky);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CAINFO, "cacert-2022-04-26.pem");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $token));
+    $result = curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if ($code == 401) {
+        return false;
+    }
+    if($code != 200) {
+        return true;
+    }
+    $js = json_decode(json_decode($result, true), true);
+    //$js[0]['total'] htmlspecialchars($js[0]['percourse'])
+    return $js;
+}
+if (!$metaStats = getStats($token, $api)) {
+    //get new token
+    require_once('newToken.php');
+    $token = newToken();
+    $metaStats = getStats($token, $api);
+}
+$metaString = "";
+if (!is_bool($metaStats)) {
+    $metaString = " with " . htmlspecialchars($js[0]['total']) . " reviews for " . htmlspecialchars($js[0]['percourse']) . " courses";
+}
+
+?>
+
+
+
+
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://n.ethz.ch/~lteufelbe/coursereview/">
+<meta property="og:title" content="CourseReview">
+<meta property="og:description" content="Site to review courses from ETHZ<?php print $metaString?>.">
+<meta property="og:image" content="https://n.ethz.ch/~lteufelbe/coursereview/icon.png">
