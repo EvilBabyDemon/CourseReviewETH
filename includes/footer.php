fb48287b5f4eea36a9d2a0229dc33fdaa29af38e
4ndriH <45722342+4ndriH@users.noreply.github.com>
2023-03-15 10:37:15 +0100

    added disclaimer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
 <div id="footer">
+    <p>CourseReview is neither supported by ETH nor by VSETH, its a private initiative run by Students</p>
     <p>If you think something is wrong or have any suggestion please contact me: <a href="mailto:lteufelbe@ethz.ch">lteufelbe@ethz.ch</a><br>
     The code is also open source: <a href="https://github.com/EvilBabyDemon/CourseReviewETH">https://github.com/EvilBabyDemon/CourseReviewETH</a></p>
 </div>
