94de08a098b492b574088e043bad9292490e8aa6
EvilBabyDemon <lteufelbe@ethz.ch>
2023-07-03 22:56:27 +0200

    no auth needed anymore

--- meta.php
+++ meta.php
@@ -1,30 +1,21 @@
 <?php
-function getMetaStats(String $token, String $api)
+function getMetaStats(String $api)
 {
     $ducky = $api . "stats";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $ducky);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_CAINFO, "cacert-2022-04-26.pem");
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $token));
     $result = curl_exec($ch);
     $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
-    if ($code == 401) {
-        return false;
-    }
     if ($code != 200) {
         return true;
     }
     $js = json_decode(json_decode($result, true), true);
     return $js;
 }
-if (!$metaStats = getMetaStats($token, $api)) {
-    //get new token
-    require_once('newToken.php');
-    $token = newToken();
-    $metaStats = getMetaStats($token, $api);
-}
+$metaStats = getMetaStats($api);
 $metaString = "";
 if (!is_bool($metaStats)) {
     $metaString = ": " . htmlspecialchars($metaStats[0]['total']) . " reviews & ratings for " . htmlspecialchars($metaStats[0]['percourse']) . " courses";
