58b397e92f100ae7f60cdceaad4424d0e39f28af
EvilBabyDemon <lteufelbe@ethz.ch>
2023-07-04 01:27:36 +0200

    formatting

--- add/index.php
+++ add/index.php
@@ -20,27 +20,130 @@ $api = trim(file_get_contents("../secret/api.txt"));
     <?php include '../includes/menu.php' ?>
     <div id="content">
         <div id="columnA">
-                <?php
-                if (isset($_POST["course"])) {
-                    $exist = true;
+            <?php
+            if (isset($_POST["course"])) {
+                $exist = true;
 
-                    //check DB if Course exists
-                    $course = $_POST["course"] . " ";
-                    $course = substr($course, 0, strpos($course, " "));
+                //check DB if Course exists
+                $course = $_POST["course"] . " ";
+                $course = substr($course, 0, strpos($course, " "));
 
-                    $db = new SQLite3('../secret/CourseReviews.db');
+                $db = new SQLite3('../secret/CourseReviews.db');
 
-                    $stmt = $db->prepare("SELECT * FROM COURSES WHERE COURSE=:course");
-                    $stmt->bindParam(':course', $course, SQLITE3_TEXT);
-                    $result = $stmt->execute();
-                    if (!$row = $result->fetchArray()) {
-                        print "<p>Are you sure this course (" . htmlspecialchars($course) . ") exists? If it does, contact me: lteufelbe@ethz.ch <br>";
-                        $exist = false;
+                $stmt = $db->prepare("SELECT * FROM COURSES WHERE COURSE=:course");
+                $stmt->bindParam(':course', $course, SQLITE3_TEXT);
+                $result = $stmt->execute();
+                if (!$row = $result->fetchArray()) {
+                    print "<p>Are you sure this course (" . htmlspecialchars($course) . ") exists? If it does, contact me: lteufelbe@ethz.ch <br>";
+                    $exist = false;
+                }
+                $db->close();
+                if ($exist) {
+
+                    function submitReview(String $ducky, String $token)
+                    {
+                        $ch = curl_init($ducky);
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+                        curl_setopt($ch, CURLOPT_POST, true);
+                        curl_setopt($ch, CURLOPT_CAINFO, "../cacert-2022-04-26.pem");
+                        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+
+                        // Set HTTP Header for POST request
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization: Bearer ' . $token));
+
+                        $result = curl_exec($ch);
+                        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                        // Close cURL session handle
+                        curl_close($ch);
+
+                        if ($code == 401) {
+                            print $result;
+                            return true;
+                        }
+                        // handle curl error
+                        if ($code != 200) {
+                            print "Error Code: $code <br>";
+                            print "Something went wrong I am sorry.";
+                        } else {
+                            if ($result == '"inserted"') {
+                                print "<h1>We received your review!</h1><p>We will verify your review to make sure it isn't attacking anyones honour.</p>";
+            ?>
+                                <script>
+                                    if (localStorage.text) {
+                                        localStorage.removeItem("text");
+                                    }
+                                </script>
+            <?php
+                            } else {
+                                print 'You already have a review for this course. Go under <a href="https://n.ethz.ch/~lteufelbe/coursereview/edit/">Edit</a> to change it.';
+                            }
+                        }
+                        return false;
                     }
-                    $db->close();
-                    if ($exist) {
 
-                        function submitReview(String $ducky, String $token)
+                    //check if there was even anything they submitted
+                    $empty = true;
+
+                    //submit a review if exists
+                    if (isset($_POST["review"]) && trim($_POST["review"]) != "") {
+                        $empty = false;
+                        $data = array(
+                            'course_id' => $course,
+                            'user_id' => $user_id,
+                            'review' => $_POST["review"]
+                        );
+                        $ducky = $api . "insertReview?";
+                        $ducky = $ducky . http_build_query($data);
+
+                        if (submitReview($ducky, $token)) {
+                            //get new token
+                            require_once('../newToken.php');
+                            $token = newToken();
+                            submitReview($ducky, $token);
+                        }
+                    }
+
+                    require_once("../submitRating.php");
+                    $ratings = ["Recommended", "Interesting", "Difficulty", "Effort", "Resources"];
+                    $ratingApi = $api . "insertRating?";
+                    //submit each rating
+                    $submittedRating = false;
+                    foreach ($ratings as $val) {
+                        if (isset($_POST[$val]) && $_POST[$val] != 0) {
+                            if ($_POST[$val] == 0) {
+                                continue;
+                            }
+                            $empty = false;
+                            $submittedRating = true;
+                            $rating = $_POST[$val];
+                            $data = array(
+                                'course_id' => $course,
+                                'user_id' => $user_id,
+                                'rating_id' => $val,
+                                'rating' => $_POST[$val]
+                            );
+
+                            $ducky = $ratingApi . http_build_query($data);
+
+                            if (submitRating($ducky, $token)) {
+                                //get new token
+                                require_once('../newToken.php');
+                                $token = newToken();
+                                submitRating($ducky, $token);
+                            }
+                        }
+                    }
+
+                    if ($submittedRating) {
+                        print "<h1>We received your ratings!</h1>";
+                    }
+
+                    if ($empty) {
+                        print "You neither submitted a review nor any ratings.";
+                    } else if (isset($_POST["semester"])) {
+                        //update Semester
+                        function updateSemester(String $ducky, String $token)
                         {
                             $ch = curl_init($ducky);
                             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -65,133 +168,29 @@ $api = trim(file_get_contents("../secret/api.txt"));
                             if ($code != 200) {
                                 print "Error Code: $code <br>";
                                 print "Something went wrong I am sorry.";
-                            } else {
-                                if ($result == '"inserted"') {
-                                    print "<h1>We received your review!</h1><p>We will verify your review to make sure it isn't attacking anyones honour.</p>";
-                ?>
-                                    <script>
-                                        if (localStorage.text) {
-                                            localStorage.removeItem("text");
-                                        }
-                                    </script>
-                <?php
-                                } else {
-                                    print 'You already have a review for this course. Go under <a href="https://n.ethz.ch/~lteufelbe/coursereview/edit/">Edit</a> to change it.';
-                                }
                             }
                             return false;
                         }
+                        $data = array(
+                            'course_id' => $course,
+                            'user_id' => $user_id,
+                            'semester' => $_POST["semester"]
+                        );
 
-                        //check if there was even anything they submitted
-                        $empty = true;
+                        $ducky = $api . "updateSemester?";
+                        $ducky = $ducky . http_build_query($data);
 
-                        //submit a review if exists
-                        if (isset($_POST["review"]) && trim($_POST["review"]) != "") {
-                            $empty = false;
-                            $data = array(
-                                'course_id' => $course,
-                                'user_id' => $user_id,
-                                'review' => $_POST["review"]
-                            );
-                            $ducky = $api . "insertReview?";
-                            $ducky = $ducky . http_build_query($data);
-
-                            if (submitReview($ducky, $token)) {
-                                //get new token
-                                require_once('../newToken.php');
-                                $token = newToken();
-                                submitReview($ducky, $token);
-                            }
+                        if (updateSemester($ducky, $token)) {
+                            //get new token
+                            require_once('../newToken.php');
+                            $token = newToken();
+                            updateSemester($ducky, $token);
                         }
-
-                        require_once("../submitRating.php");
-                        $ratings = ["Recommended", "Interesting", "Difficulty", "Effort", "Resources"];
-                        $ratingApi = $api . "insertRating?";
-                        //submit each rating
-                        $submittedRating = false;
-                        foreach ($ratings as $val) {
-                            if (isset($_POST[$val]) && $_POST[$val] != 0) {
-                                if ($_POST[$val] == 0) {
-                                    continue;
-                                }
-                                $empty = false;
-                                $submittedRating = true;
-                                $rating = $_POST[$val];
-                                $data = array(
-                                    'course_id' => $course,
-                                    'user_id' => $user_id,
-                                    'rating_id' => $val,
-                                    'rating' => $_POST[$val]
-                                );
-
-                                $ducky = $ratingApi . http_build_query($data);
-
-                                if (submitRating($ducky, $token)) {
-                                    //get new token
-                                    require_once('../newToken.php');
-                                    $token = newToken();
-                                    submitRating($ducky, $token);
-                                }
-                            }
-                        }
-
-                        if($submittedRating) {
-                            print "<h1>We received your ratings!</h1>";
-                        }
-
-                        if ($empty) {
-                            print "You neither submitted a review nor any ratings.";
-                        } else if (isset($_POST["semester"])) {
-                            //update Semester
-                            function updateSemester(String $ducky, String $token)
-                            {
-                                $ch = curl_init($ducky);
-                                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                                curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-                                curl_setopt($ch, CURLOPT_POST, true);
-                                curl_setopt($ch, CURLOPT_CAINFO, "../cacert-2022-04-26.pem");
-                                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-
-                                // Set HTTP Header for POST request
-                                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', 'Authorization: Bearer ' . $token));
-
-                                $result = curl_exec($ch);
-                                $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                                // Close cURL session handle
-                                curl_close($ch);
-
-                                if ($code == 401) {
-                                    print $result;
-                                    return true;
-                                }
-                                // handle curl error
-                                if ($code != 200) {
-                                    print "Error Code: $code <br>";
-                                    print "Something went wrong I am sorry.";
-                                }
-                                return false;
-                            }
-                            $data = array(
-                                'course_id' => $course,
-                                'user_id' => $user_id,
-                                'semester' => $_POST["semester"]
-                            );
-
-                            $ducky = $api . "updateSemester?";
-                            $ducky = $ducky . http_build_query($data);
-
-                            if (updateSemester($ducky, $token)) {
-                                //get new token
-                                require_once('../newToken.php');
-                                $token = newToken();
-                                updateSemester($ducky, $token);
-                            }
-
-                        }
-                        print "<br>";
                     }
+                    print "<br>";
                 }
-                ?>
+            }
+            ?>
             <p><b>Add a CourseReview here.</b><br>
             <form method="post" action="#">
                 <fieldset>
