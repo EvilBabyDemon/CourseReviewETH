6b1e830ab9dc553cfbf39d5f8eb0bccdf9d5d29c
EvilBabyDemon <lteufelbe@ethz.ch>
2023-09-20 15:46:03 +0200

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
     <p>CourseReview is neither supported by ETH nor by VSETH, its a private initiative run by Students.</p>
-    <p>If you think something is wrong or have any suggestion please contact me: <a href="mailto:lteufelbe@ethz.ch">lteufelbe@ethz.ch</a><br>
+    <p>If you think something is wrong or have any suggestion please contact: <a href="mailto:lteufelbe@ethz.ch">Lukas Teufelberger</a> or <a href="mailto:huandri@ethz.ch">Andri Hunkeler</a><br>
     The code is also open source: <a href="https://github.com/EvilBabyDemon/CourseReviewETH">https://github.com/EvilBabyDemon/CourseReviewETH</a></p>
 </div>
